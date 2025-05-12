8a35b4bc49113a2380b8d44d9e9c22a435cd7bb3
santiagocardenas1 <scardenas@mail.austral.edu.ar>
2025-05-12 19:06:30 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,44 @@
 # Replace the "ANSWER HERE" with your answer
 
-def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+def remove_elements(listr):
+    lista1 = listr[1:4] 
+    return lista1
 
 
-def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+def add_elements(lista):
+    lista.append("Yellow")
+    lista.insert(0, "Pink")
+    return lista
 
 
-def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+def is_empty(liste):
+    empty = []
+    comparar = empty == liste
+    return comparar
 
 
-def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+def check_lists(listch,listch1):
+    indice = 2
+    if 0 <= indice < len(listch):
+       tercero1 = listch[indice]
+    else:
+        tercero1 = []
+        indice = 2
+    if 0 <= indice < len(listch1):
+       tercero2 = listch1[indice]
+    else:
+        tercero2 = []
+    comp = tercero1 == tercero2
+    return comp
 
 
-def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+def list_of_lists(listl):
+    primero = listl[0][0:2]
+    segundo = listl[1][1:4]
+    tercero = listl[2][-2: ]
+    listlfinal = [
+        primero,
+        segundo,
+        tercero
+    ]
+    return listlfinal
